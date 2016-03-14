9ac83fd26181622bf73719afc62bbb09d85db0e2
Jonathan T. Cohen <jcohen66@optonline.net>
2016-03-14 17:55:28 -0400

    added nqueens

---
+++ .worksheet/src/week6.nqueens.scala
@@ -0,0 +1,36 @@
+package week6
+
+object nqueens {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(552); 
+  
+  def queens(n: Int): Set[List[Int]] = {
+  
+  	def placeQueens(k: Int): Set[List[Int]] =
+  		if (k == 0) Set(List())
+  		else
+  			for {
+  				queens <- placeQueens(k - 1)
+  				col <- 0 until n
+  				if isSafe(col, queens)
+  			} yield col :: queens
+
+		def isSafe(col: Int, queens: List[Int]): Boolean = {
+  		val row = queens.length
+  		val queensWithRow = (row - 1 to 0 by -1) zip queens
+  		queensWithRow forall {
+  			case (r, c) => col != c && math.abs(col - c) != row - r
+  		}
+  	}
+  			
+  	placeQueens(n)
+  };System.out.println("""queens: (n: Int)Set[List[Int]]""");$skip(192); 
+  
+  def show(queens: List[Int]) = {
+  	val lines =
+  		for (col <- queens.reverse)
+  		yield Vector.fill(queens.length)("* ").updated(col, "X ").mkString
+  		"\n" + (lines mkString "\n")
+  };System.out.println("""show: (queens: List[Int])String""");$skip(47); val res$0 = 
+  
+  (queens(8) take 3 map show) mkString "\n";System.out.println("""res0: String = """ + $show(res$0))}
+  
+}
---
+++ .worksheet/src/week6.pairs.scala
@@ -0,0 +1,28 @@
+package week6
+
+object pairs {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(88); 
+
+  def isPrime(n: Int) = (2 until n) forall (n % _ != 0);System.out.println("""isPrime: (n: Int)Boolean""");$skip(14); 
+
+  val n = 7;System.out.println("""n  : Int = """ + $show(n ));$skip(65); 
+  val xss = (1 until n) map (i => (1 until i) map (j => (i, j)));System.out.println("""xss  : scala.collection.immutable.IndexedSeq[scala.collection.immutable.IndexedSeq[(Int, Int)]] = """ + $show(xss ));$skip(14); val res$0 = 
+  xss.flatten;System.out.println("""res0: scala.collection.immutable.IndexedSeq[(Int, Int)] = """ + $show(res$0));$skip(118); 
+  val xss1 = (1 until n) flatMap (i => (1 until i) map (j => (i, j))) filter (pair =>
+    isPrime(pair._1 + pair._2));System.out.println("""xss1  : scala.collection.immutable.IndexedSeq[(Int, Int)] = """ + $show(xss1 ));$skip(87); val res$1 = 
+
+  for {
+    i <- 1 until n
+    j <- 1 until i
+    if isPrime(i + j)
+  } yield (i, j);System.out.println("""res1: scala.collection.immutable.IndexedSeq[(Int, Int)] = """ + $show(res$1));$skip(204); 
+
+  // (xs zip ys).map{ case (x, y) => x * y }.sum
+
+  def scalarProduct2(xs: Vector[Double], ys: Vector[Double]): Double =
+    (
+      for {
+        (x, y) <- (xs zip ys)
+      } yield x * y
+      ).sum;System.out.println("""scalarProduct2: (xs: Vector[Double], ys: Vector[Double])Double""")}
+
+}
