c9b40f3c5f7b9d65548394b486a2f953b6833d00
Jonathan T. Cohen <jcohen66@optonline.net>
2016-03-14 12:16:11 -0400

    mergesort with implicit arg

--- .worksheet/src/week5.mergesort.scala
+++ .worksheet/src/week5.mergesort.scala
@@ -2,10 +2,10 @@ package week5
 
 import math.Ordering
 
-object mergesort {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(580); 
+object mergesort {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(589); 
 
 
-  def msort[T](xs: List[T])(ord: Ordering[T]): List[T] = {
+  def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
@@ -24,14 +24,15 @@ object mergesort {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
       val (fst, snd) = xs splitAt n
       merge(msort(fst)(ord), msort(snd)(ord))
     }
-  };System.out.println("""msort: [T](xs: List[T])(ord: scala.math.Ordering[T])List[T]""");$skip(33); 
+  };System.out.println("""msort: [T](xs: List[T])(implicit ord: scala.math.Ordering[T])List[T]""");$skip(33); 
 
   val l1 = (1 to 10000).toList;System.out.println("""l1  : List[Int] = """ + $show(l1 ));$skip(26); val res$0 = 
-  msort(l1)(Ordering.Int);System.out.println("""res0: List[Int] = """ + $show(res$0));$skip(59); 
+  msort(l1)(Ordering.Int);System.out.println("""res0: List[Int] = """ + $show(res$0));$skip(12); val res$1 = 
+  msort(l1);System.out.println("""res1: List[Int] = """ + $show(res$1));$skip(59); 
    
    
- val fruits = List("apple", "pineapple", "orange");System.out.println("""fruits  : List[String] = """ + $show(fruits ));$skip(34); val res$1 = 
+ val fruits = List("apple", "pineapple", "orange");System.out.println("""fruits  : List[String] = """ + $show(fruits ));$skip(34); val res$2 = 
  
- msort(fruits)(Ordering.String);System.out.println("""res1: List[String] = """ + $show(res$1))}
- 
+ msort(fruits)(Ordering.String);System.out.println("""res2: List[String] = """ + $show(res$2));$skip(15); val res$3 = 
+ msort(fruits);System.out.println("""res3: List[String] = """ + $show(res$3))}
 }
--- bin/week5/mergesort.sc
+++ bin/week5/mergesort.sc
@@ -5,7 +5,7 @@ import math.Ordering
 object mergesort {
 
 
-  def msort[T](xs: List[T])(ord: Ordering[T]): List[T] = {
+  def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
@@ -41,7 +41,7 @@ object mergesort {
                                                   //| 0, 201, 202, 203, 204, 2
                                                   //| Output exceeds cutoff limit.
   msort(l1)(Ordering.Int)                         //> res0: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-                                                  //| , 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+  msort(l1)                                       //| , 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
                                                   //| , 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54
                                                   //| , 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73
                                                   //| , 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92
@@ -59,5 +59,5 @@ object mergesort {
  val fruits = List("apple", "pineapple", "orange")//> fruits  : List[String] = List(apple, pineapple, orange)
  
  msort(fruits)(Ordering.String)                   //> res1: List[String] = List(apple, orange, pineapple)
- 
+ msort(fruits)                                    //> res1: List[String] = List(apple, orange, pineapple)
 }
