3e105a1e7c4a805acf24bcec22ef8ed6f692a2ee
Jonathan T. Cohen <jcohen66@optonline.net>
2016-03-15 12:39:36 -0400

    added maps and polynomial

---
+++ .worksheet/src/week6.maps.scala
@@ -0,0 +1,14 @@
+package week6
+
+object maps {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(85); 
+
+	val romanNumerals = Map("I" -> 1, "V" -> 5, "X" -> 10);System.out.println("""romanNumerals  : scala.collection.immutable.Map[String,Int] = """ + $show(romanNumerals ));$skip(77); 
+	
+	val capitalOfCountry = Map("US" -> "Washington", "Switzerland" -> "Bern");System.out.println("""capitalOfCountry  : scala.collection.immutable.Map[String,String] = """ + $show(capitalOfCountry ));$skip(26); val res$0 = 
+	
+	capitalOfCountry("US");System.out.println("""res0: String = """ + $show(res$0));$skip(53); val res$1 = 
+
+	capitalOfCountry.getOrElse("Andorra", "Not found");System.out.println("""res1: String = """ + $show(res$1));$skip(33); val res$2 = 
+	capitalOfCountry.get("Andorra");System.out.println("""res2: Option[String] = """ + $show(res$2))}
+	
+}
---
+++ bin/week6/maps.sc
@@ -0,0 +1,19 @@
+package week6
+
+object maps {
+
+	val romanNumerals = Map("I" -> 1, "V" -> 5, "X" -> 10)
+                                                  //> romanNumerals  : scala.collection.immutable.Map[String,Int] = Map(I -> 1, V -
+                                                  //| > 5, X -> 10)
+	
+	val capitalOfCountry = Map("US" -> "Washington", "Switzerland" -> "Bern")
+                                                  //> capitalOfCountry  : scala.collection.immutable.Map[String,String] = Map(US -
+                                                  //| > Washington, Switzerland -> Bern)
+	
+	capitalOfCountry("US")                    //> res0: String = Washington
+
+	capitalOfCountry.getOrElse("Andorra", "Not found")
+                                                  //> res1: String = Not found
+	capitalOfCountry.get("Andorra")           //> res2: Option[String] = None
+	
+}
